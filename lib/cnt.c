432d0136cea6b014ff0958ca0d6a3eee176104ce
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2019-08-20 21:03:21 -0300

    Cleaning file tree

--- lib/cnt.c
+++
@@ -1,8 +0,0 @@
-int define_host_folder(char *path) {
-	DIR *d = opendir(path);
-	if(d) {
-		closedir(d);
-		return -1;
-	}
-	mkdir(path);
-}
--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include "libs"
 
 int main(int argc, char *argv[]) {
-	define_host_folder("data/test");
+
 	return 0;
 }
